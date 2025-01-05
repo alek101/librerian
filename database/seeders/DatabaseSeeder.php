9d525e066874ccf77e6b5feb02878bad2f45bc68
Aleksandar Petrovic <47416711+alek101@users.noreply.github.com>
2025-01-05 09:00:56 +0100

    setting up migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Aleksandar',
+            'surname' => 'Petrovic',
             'email' => 'test@example.com',
+            'role' => 'librerian',
         ]);
     }
 }

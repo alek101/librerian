73429253a0bb328cff9ceda4e1830f12de6a40df
Aleksandar Petrovic <47416711+alek101@users.noreply.github.com>
2025-01-05 10:50:47 +0100

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'surname' => fake()->surname(),
+            'surname' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
